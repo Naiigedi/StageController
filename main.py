dcf02cb84db32659c6a4608c2d89be138c7243c8
taichi <taichi.matsumori@goodanswers.inc>
2021-10-12 16:46:00 +0900

    ADD:  calculate RPM and deg from parameter1&2

--- main.py
+++ main.py
@@ -13,7 +13,13 @@ ccwpin.freq(int(frequency))
 def deg2time(deg): # this motor speed is 10RPM.
 	# 若干誤差があるため、補正係数(実測により求めた)を入れる
 	correction = 0.8
-	return deg/(60 + correction)
+	return deg/(360 * 10 / 60 + correction)
+
+
+def deg2timeRpm(deg, rpm): # this motor speed is 0~10RPM.
+	# 若干誤差があるため、補正係数を入れる予定（未計算）
+	correction = 0
+	return deg/(360 * int(rpm) / 60 + correction)
 
 def cw():
 	cwpin.duty_u16(0xFFFF)
@@ -22,6 +28,18 @@ def cw():
 def ccw():
 	cwpin.duty_u16(0)
 	ccwpin.duty_u16(0xFFFF)
+
+def cw_rpm(rpm):
+	rpm = int(rpm)
+	duty = int(rpm / 10 * 0xFFFF)
+	cwpin.duty_u16(duty)
+	ccwpin.duty_u16(0)
+
+def ccw_rpm(rpm):
+	rpm = int(rpm)
+	duty = int(rpm / 10 * 0xFFFF)
+	cwpin.duty_u16(0)
+	ccwpin.duty_u16(duty)
 
 def brake():
 	cwpin.duty_u16(0xFFFF)
@@ -32,19 +50,25 @@ def wait_rotate(deg_list):
 	rotation_time = deg2time(deg)
 	utime.sleep(rotation_time)
 
+def wait_rotate_rpm(deg_list, rpm):
+	deg = float(deg_list)
+	rotation_time = deg2timeRpm(deg, rpm)
+	utime.sleep(rotation_time)
 
 
 print("please input command and parameter.")
-print("CW XXX   rotation stage clockwise. XXX is degree(0~360)")
-print("CCW XXX  rotation stage counterclockwise. XXX is degree(0~360)")
-print("The rotation default speed of this motor is 10RPM.")
+print("CW XXX [YYY]  rotation stage clockwise. XXX is degree(0~360). YYY is RPM(default 10RPM)")
+print("CCW XXX [YYY] rotation stage counterclockwise. XXX is degree(0~360). YYY is RPM(default 10RPM)")
 
 while True:
 	commands = input()
 	
 	command_list = commands.split(" ")
 	command_id = command_list[0]
-	if(len(command_list)<1):
+
+	print(len(command_list))
+	# for arg: 0
+	if(len(command_list)==1):
 		if  command_id == "LON":	# for debug
 			led.value(1)
 			print("LED ON")
@@ -53,7 +77,9 @@ while True:
 			led.value(0)
 			print("LED OFF")
 
-	else:
+
+	# for arg: 1
+	elif(len(command_list)== 2):
 		if command_id == "CW":
 			if(command_list[1].isdigit()):
 				print("rotating CW", command_list[1])	
@@ -82,5 +108,45 @@ while True:
 				print("please input number for the second argument")
 
 
+	# for arg: 2
+	elif(len(command_list)== 3):
+		param1 = command_list[1]
+		param2 = command_list[2]
+
+		if command_id == "CW":
+			if(param1.isdigit() and param2.isdigit()):
+				print("rotating CW", param1)	
+				led.value(1)
+
+				# 　ここに時計回りの動作を記入する
+				if(float(param2) > 0 or float(param2) < 10):
+					cw_rpm(param2)
+					wait_rotate_rpm(param1,param2)
+					brake()
+					led.value(0)
+				else:
+					print("please input RPM 0 ~ 10")
+			else:
+				print("please input number for the second argument")
+
+
+		elif command_id == "CCW":
+			if(param1.isdigit() and param2.isdigit()):
+				print("rotating CCW", param1)
+				led.value(1)
+
+				# 　ここに時計回りの動作を記入する
+				if(float(param2) > 0 or float(param2) < 10):
+					ccw_rpm(param2)
+					wait_rotate_rpm(param1,param2)
+					brake()
+					led.value(0)
+				else:
+					print("please input RPM 0 ~ 10")
+			else:
+				print("please input number for the second argument")
+
+	
+		# for arg > 2 (error cases)
 		else:
 			print("This is an undefined command id. Please retype it")
