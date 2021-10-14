fb5f3cac602bb62e8fde5049ada6295b3134e7aa
taichi <taichi.matsumori@goodanswers.inc>
2021-10-14 11:24:35 +0900

    FIX: can input float number and fix devide error

--- main.py
+++ main.py
@@ -19,7 +19,7 @@ def deg2time(deg): # this motor speed is 10RPM.
 def deg2timeRpm(deg, rpm): # this motor speed is 0~10RPM.
 	# Since there is an error, a correction factor will be included (not yet calculated).
 	correction = 0
-	return deg/(360 * int(rpm) / 60 + correction)
+	return deg/(360 * rpm / 60 + correction)
 
 def cw():
 	cwpin.duty_u16(0xFFFF)
@@ -30,13 +30,13 @@ def ccw():
 	ccwpin.duty_u16(0xFFFF)
 
 def cw_rpm(rpm):
-	rpm = int(rpm)
+	rpm = rpm
 	duty = int(rpm / 10 * 0xFFFF)
 	cwpin.duty_u16(duty)
 	ccwpin.duty_u16(0)
 
 def ccw_rpm(rpm):
-	rpm = int(rpm)
+	rpm = rpm
 	duty = int(rpm / 10 * 0xFFFF)
 	cwpin.duty_u16(0)
 	ccwpin.duty_u16(duty)
@@ -44,17 +44,27 @@ def ccw_rpm(rpm):
 def brake():
 	cwpin.duty_u16(0xFFFF)
 	ccwpin.duty_u16(0xFFFF)
+	print("rotation finish")
 
 def wait_rotate(deg_list):
-	deg = float(deg_list)
+	deg = deg_list
 	rotation_time = deg2time(deg)
 	utime.sleep(rotation_time)
 
 def wait_rotate_rpm(deg_list, rpm):
-	deg = float(deg_list)
+	deg = deg_list
 	rotation_time = deg2timeRpm(deg, rpm)
 	utime.sleep(rotation_time)
 
+
+def check_param_float(param):
+	try:
+		param = float(param)
+	except ValueError:
+		print("Value error")
+		return False
+	else:
+		return param
 
 print("please input command and parameter.")
 print("CW XXX [YYY]  rotation stage clockwise. XXX is degree(0~360). YYY is RPM(default 10RPM)")
@@ -80,67 +90,74 @@ while True:
 
 	# for arg: 1
 	elif(len(command_list)== 2):
+		param1 = command_list[1]
+		param1_float = check_param_float(param1)
+
 		if command_id == "CW":
-			if(command_list[1].isdigit()):
-				print("rotating CW", command_list[1])	
+			if(param1_float == False):
+				print("please input number for the second argument")
+			else:
+				print("rotating CW", param1_float, "deg")	
 				led.value(1)
 
 				cw()
-				wait_rotate(command_list[1])
+				wait_rotate(param1_float)
 				brake()
 				led.value(0)
-			else:
-				print("please input number for the second argument")
 
 
 		elif command_id == "CCW":
-			if(command_list[1].isdigit()):
-				print("rotating CCW", command_list[1])
+			if(param1_float == False):
+				print("please input number for the second argument")
+			else:
+				print("rotating CCW", param1_float, "deg")
 				led.value(1)
 
 				ccw()
-				wait_rotate(command_list[1])
+				wait_rotate(param1_float)
 				brake()
 				led.value(0)
-			else:
-				print("please input number for the second argument")
 
 
 	# for arg: 2
 	elif(len(command_list)== 3):
 		param1 = command_list[1]
 		param2 = command_list[2]
+		param1_float = check_param_float(param1)
+		param2_float = check_param_float(param2)
 
 		if command_id == "CW":
-			if(param1.isdigit() and param2.isdigit()):
-				print("rotating CW", param1)	
+			if(param1_float == False or param2_float == False):
+				print("please input number for the second argument")
+			
+			else:
+				print("rotating CW", param1_float, "deg at", param2_float, "RPM")	
 				led.value(1)
 
-				if(float(param2) > 0 or float(param2) < 10):
-					cw_rpm(param2)
-					wait_rotate_rpm(param1,param2)
+				if(param2_float > 0 or param2_float < 10):
+					cw_rpm(param2_float)
+					wait_rotate_rpm(param1_float, param2_float)
 					brake()
 					led.value(0)
 				else:
 					print("please input RPM 0 ~ 10")
-			else:
-				print("please input number for the second argument")
 
 
 		elif command_id == "CCW":
-			if(param1.isdigit() and param2.isdigit()):
-				print("rotating CCW", param1)
+			if(param1_float == False or param2_float == False):
+				print("please input number for the second argument")
+			
+			else:
+				print("rotating CCW", param1_float, "deg at", param2_float, "RPM")	
 				led.value(1)
 
-				if(float(param2) > 0 or float(param2) < 10):
-					ccw_rpm(param2)
-					wait_rotate_rpm(param1,param2)
+				if(param2_float > 0 or param2_float < 10):
+					ccw_rpm(param2_float)
+					wait_rotate_rpm(param1_float, param2_float)
 					brake()
 					led.value(0)
 				else:
 					print("please input RPM 0 ~ 10")
-			else:
-				print("please input number for the second argument")
 
 	
 		# for arg > 2 (error cases)
